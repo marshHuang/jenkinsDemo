208372f5b7a21fe56be7dc93f1d7f3ce8d918c8f
marsh <marsh.jianghu@gmail.com>
2021-01-07 23:01:53 +0800

    '更新2'

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="hello">
-      HelloWorld! 自动更新！---重装了!现在可以自动部署到服务器了！——-！！！！1111更新了！－－－－ 再次更新1
+      HelloWorld! 自动更新！---重装了!现在可以自动部署到服务器了！——-！！！！1111更新了！－－－－ 再次更新2
     <div class="download">
-      <a href="../assets/music.mp3" download="music.mp3">点击下载</a>
+      <a href="https://oss.hdyl.net.cn/files/541347453287329792.mp3" download="music.mp3">点击下载</a>
     </div>
   </div>
 </template>
